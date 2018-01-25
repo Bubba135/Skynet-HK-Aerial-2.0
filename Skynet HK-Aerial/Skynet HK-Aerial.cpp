60c1ebb9438153aef747fc17f6f601f2b1abdc16
CRISTIAN FLORES-FOSTER <CRIFLORE@uat.edu>
2018-01-25 16:55:48 -0700

    New Code added.

--- Skynet HK-Aerial/Skynet HK-Aerial.cpp
+++ Skynet HK-Aerial/Skynet HK-Aerial.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
-//coment
-//hi
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
 int main()
 {
-    return 0;
+	srand(static_cast<unsigned int>(time(0)));
+
+	int secretNumber = rand() % 100 + 1;
+
+	int tries = 0;
+	int guess;
+
+	cout << "\tWelcome to guess My Number\n\n";
+
+	do
+	{
+		cout << "Enter a guess: ";
+		cin >> guess;
+
+		if (guess > secretNumber)
+		{
+			cout << "Too high!\n\n";
+		}
+		else if (guess < secretNumber)
+		{
+			cout << "Too low\n\n";
+		}
+		else
+		{
+			cout << "\n That's it! You got it in " << tries << " guesses!\n";
+		}
+	} while (guess != secretNumber);
+    
+	system("pause");
+
+	return 0;
 }
 
