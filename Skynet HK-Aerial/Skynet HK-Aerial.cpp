2329fa8f9a5e3e8a1c6762abd1fe28c8a58798fc
CRISTIAN FLORES-FOSTER <criflore@PLAZ01>
2018-01-25 18:10:40 -0700

    Sum of the final code left.

--- Skynet HK-Aerial/Skynet HK-Aerial.cpp
+++ Skynet HK-Aerial/Skynet HK-Aerial.cpp
@@ -16,22 +16,39 @@ int main()
 	//Add High and Low Vars
 	int tries = 0;
 	int guess;
+	int highRange = 64;
+	int lowRange = 1;
 
 	cout << "\tWelcome to guess My Number\n\n";
 
+	cout << "The is hiding out in Range: " << secretNumber << " in a 8x8 grid." << endl;
+
 	do
 	{
-		cout << "Enter a guess: ";
-		cin >> guess;
+		cout << endl << endl;
+		cout << "***************************" << endl;
+		cout << "Enter a guess: " << endl;
+		guess = (highRange - lowRange) / 2 + lowRange;
+		cout << "AI has a Guess of: " << guess << endl;
 		++tries;
 
 		if (guess > secretNumber)
 		{
 			cout << "Too high!\n\n";
+			cout << "AI augsting High Range" << endl;
+			cout << "Old High range is " << highRange << endl;
+			//change the high keep the low
+			highRange = guess - 1;
+			cout << "New High range is " << highRange << endl;
 		}
 		else if (guess < secretNumber)
 		{
 			cout << "Too low\n\n";
+			cout << "AI augsting Low Range" << endl;
+			cout << "Old Low range is " << lowRange << endl;
+			//change loww keep the high
+			lowRange = guess + 1;
+			cout << "New Low range is " << lowRange << endl;
 		}
 		else
 		{
