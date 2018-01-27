b27f7b71c6b7954e275cd5383d9c383dd94710e1
CRISTIAN FLORES-FOSTER <criflore@PLAZ54>
2018-01-27 14:55:53 -0700

    The final work.

--- Skynet HK-Aerial/Skynet HK-Aerial.cpp
+++ Skynet HK-Aerial/Skynet HK-Aerial.cpp
@@ -19,12 +19,15 @@ int main()
 	int highRange = 64;
 	int lowRange = 1;
 
-	cout << "\tWelcome to guess My Number\n\n";
+	cout << "\tWelcome to a new drown hunting program.\n\n";
 
-	cout << "The is hiding out in Range: " << secretNumber << " in a 8x8 grid." << endl;
+	cout << "In this program you will see how a AI will hunt down enemy in a 8x8 grid with out humen erer." << endl;
+
+	cout << "The eneny is hiding out in Range: " << secretNumber << " in a 8x8 grid." << endl;
 
 	do
 	{
+		//How the program will find it numbers
 		cout << endl << endl;
 		cout << "***************************" << endl;
 		cout << "Enter a guess: " << endl;
@@ -34,6 +37,7 @@ int main()
 
 		if (guess > secretNumber)
 		{
+			//The code for being to High
 			cout << "Too high!\n\n";
 			cout << "AI augsting High Range" << endl;
 			cout << "Old High range is " << highRange << endl;
@@ -43,15 +47,17 @@ int main()
 		}
 		else if (guess < secretNumber)
 		{
+			// The code for being to low.
 			cout << "Too low\n\n";
 			cout << "AI augsting Low Range" << endl;
 			cout << "Old Low range is " << lowRange << endl;
-			//change loww keep the high
+			//change low keep the high
 			lowRange = guess + 1;
 			cout << "New Low range is " << lowRange << endl;
 		}
 		else
 		{
+			// If it gets the number.
 			cout << "\n That's it! You got it in " << tries << " guesses!\n";
 		}
 	} while (guess != secretNumber);
@@ -60,4 +66,3 @@ int main()
 
 	return 0;
 }
-
